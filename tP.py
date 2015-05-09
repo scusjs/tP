76cd02363e64fbfc55fccaa302e1b9e38fb58329
scusjs <scusjs@foxmail.com>
2015-05-09 22:40:17 +0800

    update a lot

--- tP.py
+++ tP.py
@@ -15,6 +15,8 @@ import locale
 locale.setlocale(locale.LC_ALL, '')
 default_line_time = 1
 
+presentation_content = [{"content" : [{"content":"test", "attribute" : ""}, {"content" : "你好世界" , "attribute" : ""}], "attribute" : "body"}, {"content" : [{"content" : "int a;", "attribute" : ""}, {"content" : "int b;", "attribute" : ""}], "attribute" : "code"}, {"content" : [{"content" : "scu.jpg", "attribute" : ""}], "attribute" : "image"}, {"content" : [{"content" : "hello world", "attribute" : ""}], "attribute" : "session"}]
+
 class TerminalPresentation:
     def __init__(self):
         self.currentpath = os.getcwd()
@@ -22,16 +24,24 @@ class TerminalPresentation:
         curses.cbreak()
         self.height, self.width = self.screen.getmaxyx()
         self.show_str(" ", 0, True)
-        self.show_str("test", 5, True)
-        self.show_str("你好世界", 6, True)
-        self.show_str("======❧❦☙=====", 7, True)
-        self.__clr()
-        self.__set_background("scu.jpg")
-        sleep(2)
-        self.__reset_background()
-        self.show_str("这是第二页", 7, True)
+        for i in presentation_content:
+            self.show_page(i)
         self.screen.getch()
         curses.endwin()
+
+    def show_page(self, page_info):
+        page_attribute = page_info["attribute"]
+        page_content = page_info["content"]
+        self.__clr()
+        #if page_attribute == "session":
+        if True:
+            start_y = self.__get_start_y(len(page_content) + 2)
+            self.show_str("======❧❦☙======", start_y, False)
+            self.show_str("======❧❦☙======", start_y + len(page_content) + 1, False)
+            for i in range(0, len(page_content)):
+                content_txt = page_content[i]["content"]
+                self.show_str(content_txt, start_y + i + 1, True)
+
 
     def show_str(self, printstr, height, sleep):
         start_x = self.__get_start_x(printstr)
@@ -78,6 +88,9 @@ class TerminalPresentation:
             if self.__is_cn_char(printstr[i]):
                 start_x += 1
         return int((self.width - start_x) / 2)
+
+    def __get_start_y(self, content_len):
+        return int((self.height - content_len) / 2)
 
 
 if __name__ == "__main__":
